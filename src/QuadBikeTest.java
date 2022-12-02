1cee0bb6d0650a9c5d3b2cc8e4f2ae3a6c413d0b
DePono <109430169+DePono@users.noreply.github.com>
2022-12-02 20:27:55 +0400

    Добавлен QuadBikeTest

--- src/QuadBikeTest.java
+++ src/QuadBikeTest.java
@@ -2,11 +2,20 @@ import IVT.Exeptions.DuplicateModelNameException;
 import IVT.Exeptions.NoSuchModelNameException;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class QuadBikeTest {
-    public static void main(String[] args) throws NoSuchModelNameException, DuplicateModelNameException {
-        Vehicle quadBike = new QuadBike("Porsche",6);
+    public static void main(String[] args) throws DuplicateModelNameException, NoSuchModelNameException {
+        QuadBike quadBike = new QuadBike("Polo",2);
+        System.out.println(quadBike);
+        quadBike.setModelName("Polo1","Poller4");
+        System.out.println(quadBike);
+        System.out.println(Arrays.toString(quadBike.getAllModelNames()));
+        System.out.println(quadBike.getPriceModelByName("Polo0"));
+        quadBike.setPriceModelByName("Polo0",1000);
+        System.out.println(quadBike);
+        System.out.println(Arrays.toString(quadBike.getAllModelPrices()));
+        quadBike.addModel("Lop",323);
+        System.out.println(quadBike);
         System.out.println(quadBike);
     }
 }
